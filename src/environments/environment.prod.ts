f13b70c807d4c63ebd4e02728a23452a119cfa20
Ethan De Guzman <S00199053@mail.itsligo.ie>
2021-03-31 13:46:54 +0100

    Hosted to firebase and fixed deploy bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyAmja-ax4DDt9G5CXnCBLkviHhKKL1SipA',
-    authDomain: 'mycarsales-26e71.firebaseapp.com',
-    projectID: 'mycarsales-26e71',
-    storageBucket: 'mycarsales-26e71.appspot.com',
-    messagingSenderId: '542577596634',
-    appId: '1:542577596634:web:9ec2b36cab4b8ae11b2de3'
-  }
+    apiKey: "AIzaSyAmja-ax4DDt9G5CXnCBLkviHhKKL1SipA",
+    authDomain: "mycarsales-26e71.firebaseapp.com",
+    databaseURL: "https://mycarsales-26e71-default-rtdb.firebaseio.com",
+    projectId: "mycarsales-26e71",
+    storageBucket: "mycarsales-26e71.appspot.com",
+    messagingSenderId: "542577596634",
+    appId: "1:542577596634:web:4c057a7d26e98f9b1b2de3"
+    }
 };
